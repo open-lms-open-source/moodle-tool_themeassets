c55ca2f999fdb0173306f09493605dcb71beebb9
Fabian Batioja <fabian.batioja@openlms.net>
2021-09-20 16:03:46 +0000

    INT-17437: Bump versions for 3.11: After merge

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2021060100; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2020110900; // Requires this Moodle version (3.3.1).
+$plugin->requires  = 2021051700; // Requires this Moodle version (3.3.1).
 $plugin->component = 'tool_themeassets'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.10.3';
+$plugin->release   = '3.11.2';
