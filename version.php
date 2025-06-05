8d9ac34b8479e7edce028d4908e160245b048b1a
Daniel Cifuentes <daniel.cifuentes@openlms.net>
2025-06-05 16:40:33 -0500

    INT-20930: Bump version 4.5 SB

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025031800; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2024042200; // Requires this Moodle version (3.3.1).
+$plugin->version   = 2025060500; // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2024100700; // Requires this Moodle version (3.3.1).
 $plugin->component = 'tool_themeassets'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '4.4.4';
+$plugin->release   = '4.5.4';
